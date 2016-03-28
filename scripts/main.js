88cc7dd43d6e1c52017bff1bbb54f2fd70613ebd
limzykenneth <limzy.kenneth@gmail.com>
2016-03-28 16:31:21 +0100

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,24 @@
+function start(e) {
+	var val = $("#calc").val();
+    worker.postMessage({
+        'cmd': 'start',
+        'msg': 'Hi',
+        'dec': val
+    });
+}
+
+function stop(e) {
+    // worker.terminate() from this script would also stop the worker.
+    worker.postMessage({
+        'cmd': 'stop',
+        'msg': 'Bye'
+    });
+}
+var worker = new Worker('./scripts/task.js');
+worker.addEventListener('message', function(e) {
+    $("#result").html(e.data);
+}, false);
+$(document).ready(function() {
+    $("#start").click(start);
+    $("#stop").click(stop);
+});
