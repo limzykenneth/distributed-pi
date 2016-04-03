30420f57063f47acd6d2622a7727e20ae8c97e41
limzykenneth <limzy.kenneth@gmail.com>
2016-04-03 22:28:23 +0100

    Hard code mysql settings

--- writePI.js
+++ writePI.js
@@ -5,9 +5,9 @@ var writePI = writePI || {};
 
 
 var connection = mysql.createConnection({
-  host     : process.env.mysql_host,
-  user     : process.env.mysql_user,
-  password : process.env.mysql_password,
+  host     : "eu-cdbr-west-01.cleardb.com",//process.env.mysql_host,
+  user     : "bc968df427b9f3",//process.env.mysql_user,
+  password : "f59d38db",//process.env.mysql_password,
   database : 'distributed_pi',
   bigNumberStrings: true,
   supportBigNumbers: true
